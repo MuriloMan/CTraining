5881f6616f981fd7e143e7bc6cc34aefdb361cb6
AlisonFrancisco <alisoncontatprofissional@gmail.com>
2020-12-11 11:03:40 -0300

    Inserted the survivor struct.

--- program.c
+++ program.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct survivor{
+    struct survivor *helperPointer;
+    int survivorID;
+    int coltQuantity;
+    int shotgunQuantity;
+    float coltDamage;
+    float shotgunDamage;
+};
+
+int main(){
+
+}
