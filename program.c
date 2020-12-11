30e664cbd8827058534116a93db98d0508aad8c3
AlisonFrancisco <alisoncontatprofissional@gmail.com>
2020-12-11 11:50:11 -0300

    Inserted the survivor list method.

--- program.c
+++ program.c
@@ -2,13 +2,40 @@
 #include <stdlib.h>
 
 struct survivor{
-    struct survivor *helperPointer;
+    struct survivor *nextPointer;
     int survivorID;
     int coltQuantity;
     int shotgunQuantity;
     float coltDamage;
     float shotgunDamage;
 };
+
+struct survivor *topPointer;
+
+int totalSurvivors = 0;
+int lastSurvivorID = 1;
+
+void addSurvivor(){
+    struct survivor *addHelper = topPointer;
+}
+
+void removeSurvivor(){ 
+    struct survivor *removeHelper = topPointer;
+}
+
+void survivorsList(){
+    if(totalSurvivors == 0){
+        printf("\n Error = The pile is empty. \n");
+    }else{
+        struct survivor *listHelper = topPointer;
+        printf("\n Survivor ID = %d | Colt quantity = %d | Colt damage = %d | Shotgun quantity = %d | Shotgun damage = %d \n",listHelper->survivorID,listHelper->coltQuantity,listHelper->coltDamage,listHelper->shotgunQuantity,listHelper->shotgunDamage);
+        listHelper = listHelper->nextPointer;
+    }
+}
+
+void updateSurvivor(){
+    
+}
 
 int main(){
 
