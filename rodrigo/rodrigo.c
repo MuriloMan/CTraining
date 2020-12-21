995b34ef4a6c8f564b6a20471a2d419bc0899427
RodrigoMan <rodrigo.fessore@gmail.com>
2020-12-21 11:44:09 -0300

    Implementando funcinalidade de listagem na pilha

--- rodrigo/rodrigo.c
+++ rodrigo/rodrigo.c
@@ -1,46 +1,76 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct pessoa{
+struct pessoa
+{
     int rg;
     int cpf;
     struct pessoa *prox;
 };
 
-int totalPessoas= 0;
+int totalPessoas = 0;
 struct pessoa *topo;
 
-int menu(){
+int menu()
+{
     int op;
     printf("\nDigite 1- Inserir, 5- Sair\n");
     scanf("%d, &op");
     return op;
 }
 
-void inserir(){
-    struct pessoa *aux=(struct pessoa*)malloc(sizeof(struct pessoa));
+void inserir()
+{
+    struct pessoa *aux = (struct pessoa *)malloc(sizeof(struct pessoa));
     printf("\nDigite o rg da pessoa\n");
     scanf("%d", &aux->rg);
     printf("\nDigite o cpf da pessoa\n");
     scanf("%d", &aux->cpf);
-    if(totalPessoas == 0){
-        topo= aux;
-    }else{
-        aux->prox=topo;
-        topo=aux;
+    if (totalPessoas == 0)
+    {
+        topo = aux;
     }
-    totalPessoas ++;
+    else
+    {
+        aux->prox = topo;
+        topo = aux;
+    }
+    totalPessoas++;
 }
 
-int main(){
+void listar()
+{
+    if (totalPessoas == 0)
+    {
+        printf("\nA pilha está vazia\n");
+    }
+    else
+    {
+        struct pessoa *aux= topo;
+        while (aux != NULL)
+        {
+            printf("\n%d %d\n", aux->rg, aux->cpf);
+            aux = aux->prox;
+        }
+    }
+}
+
+int main()
+{
     int op;
 
-    do{
-        op =menu();
-        if (op == 1){
+    do
+    {
+        op = menu();
+        if (op == 1)
+        {
             inserir();
         }
-    }while(op != 5);
+        else if (op == 2)
+        {
+            listar();
+        }
+    } while (op != 5);
 
     return 0;
 }
