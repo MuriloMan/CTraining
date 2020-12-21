b5e68879a654a6d39f59018e242f68c2a19f7b1c
MuriloMan <manzolimurilo@gmail.com>
2020-12-21 11:33:17 -0300

    Create projeto.c

---
+++ projeto.c
@@ -0,0 +1,8 @@
+//Criar uma pilha
+//Alison faz o struct, listar e criar
+//Rodrigo faz o main, Atualizar e deletar
+//Necessário fazer tudo e comitar depois, só imaginando como será o trabalho do outro
+
+#include <stdio.h>
+
+//CODE
